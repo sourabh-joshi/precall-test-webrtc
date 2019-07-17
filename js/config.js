8b7e4a6e4d2bc22cc25fae1f9e00e1bf4174fbac
Sourabh Joshi <sourabhjoshi436@gmail.com>
2019-07-17 22:40:08 +0530

    Config file for iceservers

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const iceServers = [];
+
+var PACKET_LOSS_THRESHOLD = 4; //In %
+var AVERAGE_BITRATE_THRESHOLD = 1000; //In kbps
+var RTT_THRESHOLD = 300; // In milliseconds
+
+var VIDEO_TEST_DURATION_MS = 10000;
+var THROUGHPUT_TEST_DURATION_MS = 5000;
